1f25a4e7bbf4c4a9dbb6d2155b857ac094c1e4fb
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-11-04 09:45:30 -0500

    Initial Commit

---
+++ NikitasMarksChart/NikitasMarksChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikitasMarksChartApp.swift
+//  NikitasMarksChart
+//
+//  Created by Nikita Zhdanov on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct NikitasMarksChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NikitasMarksChartTests/NikitasMarksChartTests.swift
@@ -0,0 +1,17 @@
+//
+//  NikitasMarksChartTests.swift
+//  NikitasMarksChartTests
+//
+//  Created by Nikita Zhdanov on 2024-11-04.
+//
+
+import Testing
+@testable import NikitasMarksChart
+
+struct NikitasMarksChartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
